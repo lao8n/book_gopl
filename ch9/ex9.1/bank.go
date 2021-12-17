deb00925b2eb332dd1b06798230f4fa57104a77a
Nicholas Drake Deliveroo <hellolao8n@gmail.com>
2021-12-17 18:29:07 +0000

    ex9.1 withdraw avoid race conditions

--- ch9/ex9.1/bank.go
+++ ch9/ex9.1/bank.go
@@ -7,14 +7,23 @@
 // Package bank provides a concurrency-safe bank with one account.
 package bank
 
-var deposits = make(chan int) // send amount to deposit
-var balances = make(chan int) // receive balance
-var withdraw = make(chan int) //
-var confirm = make(chan bool)
+type withdrawRequest struct {
+	amount  int
+	confirm chan<- bool
+}
 
-func Deposit(amount int)       { deposits <- amount }
-func Balance() int             { return <-balances }
-func Withdraw(amount int) bool { withdraw <- amount; withdrawFlag := <-confirm; return withdrawFlag }
+var deposits = make(chan int)             // send amount to deposit
+var balances = make(chan int)             // receive balance
+var withdraw = make(chan withdrawRequest) //
+
+func Deposit(amount int) { deposits <- amount }
+func Balance() int       { return <-balances }
+func Withdraw(amount int) bool {
+	// need a separate confirmation for every withdrawal
+	confirm := make(chan bool)
+	withdraw <- withdrawRequest{amount: amount, confirm: confirm}
+	return <-confirm // blocks
+}
 
 func teller() {
 	var balance int // balance is confined to teller goroutine
@@ -22,14 +31,13 @@ func teller() {
 		select {
 		case amount := <-deposits:
 			balance += amount
-		case amount := <-withdraw:
-			if amount <= balance {
-				balance -= amount
-				confirm <- true
+		case req := <-withdraw:
+			if req.amount <= balance {
+				balance -= req.amount
+				req.confirm <- true
 			} else {
-				confirm <- false
+				req.confirm <- false
 			}
-			balance -= amount
 		case balances <- balance:
 		}
 	}
--- ch9/ex9.1/bank_test.go
+++ ch9/ex9.1/bank_test.go
@@ -1,36 +1,87 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-package bank_test
+package bank
 
 import (
-	"fmt"
+	"sync"
 	"testing"
-
-	"gopl.io/ch9/bank1"
 )
 
+type BankOperation struct {
+	op     string // "deposit", "withdraw", "balance"
+	amount int    // ignored when op == "balance"
+	ok     bool   // expect ok or not. only when op == "withdraw"
+}
+
 func TestBank(t *testing.T) {
-	done := make(chan struct{})
+	tests := []struct {
+		operations []BankOperation
+		init       int
+		want       int
+	}{
+		{
+			[]BankOperation{
+				BankOperation{"withdraw", 1, true},
+			},
+			10,
+			9,
+		},
+		{
+			[]BankOperation{
+				BankOperation{"withdraw", 1, true},
+				BankOperation{"withdraw", 2, true},
+				BankOperation{"withdraw", 3, true},
+				BankOperation{"withdraw", 4, true},
+			},
+			10,
+			0,
+		},
+		{
+			[]BankOperation{
+				BankOperation{"withdraw", 1, true},
+				BankOperation{"withdraw", 100, false},
+			},
+			10,
+			9,
+		},
+		{
+			[]BankOperation{
+				BankOperation{"deposit", 1, true},
+				BankOperation{"deposit", 1, true},
+				BankOperation{"withdraw", 1, true},
+				BankOperation{"withdraw", 1, true},
+				BankOperation{"balance", 1, true},
+				BankOperation{"balance", 1, true},
+			},
+			2,
+			2,
+		},
+	}
 
-	// Alice
-	go func() {
-		bank.Deposit(200)
-		fmt.Println("=", bank.Balance())
-		done <- struct{}{}
-	}()
+	for _, test := range tests {
+		// init balance
+		Withdraw(Balance() - test.init)
 
-	// Bob
-	go func() {
-		bank.Deposit(100)
-		done <- struct{}{}
-	}()
-
-	// Wait for both transactions.
-	<-done
-	<-done
-
-	if got, want := bank.Balance(), 300; got != want {
-		t.Errorf("Balance = %d, want %d", got, want)
+		var wg sync.WaitGroup
+		for _, operation := range test.operations {
+			operation := operation
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				switch operation.op {
+				case "deposit":
+					Deposit(operation.amount)
+				case "withdraw":
+					ok := Withdraw(operation.amount)
+					if ok != operation.ok {
+						t.Errorf("Withdraw(%d) in %v = %t, want %t", operation.amount, test.operations, ok, operation.ok)
+					}
+				case "balance":
+					Balance()
+				}
+			}()
+		}
+		wg.Wait()
+		if got := Balance(); got != test.want {
+			t.Errorf("Balance() after %v = %d, want %d", test.operations, got, test.want)
+		}
 	}
 }
